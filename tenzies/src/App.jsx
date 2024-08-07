7ccdf05a8cbe60c51dbcfd4c1d2b24f9d1fe2f5e
deordiev-dm <dimadeordiev2002@gmail.com>
2024-08-07 13:43:31 +0300

    completed winning and reseting logic

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { nanoid } from "nanoid";
 import Die from "./components/Die";
+import Confetti from "react-confetti";
 
 const NUMBER_OF_DICE = 10;
 
 function App() {
   // state variable declarations
   const [dice, setDice] = useState(initDice);
+  const [tenzies, setTenzies] = useState(false);
+
+  // effects
+  useEffect(() => {
+    if (dice.length === 0) return;
+
+    const firstDieValue = dice[0].value;
+    const isWin = dice.every(die => die.value === firstDieValue && die.isHeld);
+
+    if (isWin) {
+      setTenzies(true);
+    }
+  }, [dice]);
 
   // generating elements
   const diceComponents = dice.map((die, index) => (
@@ -22,23 +36,33 @@ function App() {
   // returning the markup
   return (
     <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="subtitle">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{diceComponents}</div>
       <button
         className="reroll-button"
         onClick={rerollDice}
       >
-        Roll
+        {tenzies ? "New game" : "Roll"}
       </button>
     </main>
   );
 
   // event handlers
   function rerollDice() {
-    setDice(prevDice =>
-      prevDice.map(die => {
-        return die.isHeld ? die : { ...die, value: generateRandomDiceRoll() };
-      })
-    );
+    if (tenzies) {
+      resetGame();
+    } else {
+      setDice(prevDice =>
+        prevDice.map(die => {
+          return die.isHeld ? die : { ...die, value: generateRandomDiceRoll() };
+        })
+      );
+    }
   }
 
   function holdDie(id) {
@@ -50,6 +74,11 @@ function App() {
   }
 
   // helper functions
+  function resetGame() {
+    setTenzies(false);
+    setDice(initDice());
+  }
+
   function initDice() {
     let dice = [];
 
